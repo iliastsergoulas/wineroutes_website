a544448cc9a7101258175b87ef2c60b345be9201
Ilias Tsergoulas <htsergoulas@yahoo.gr>
2023-05-28 12:16:12 +0300

    First commit

---
+++ js/languages.js
@@ -0,0 +1,54 @@
+var greeknames = ["Δεν έχετε εισάγει έγκυρη διεύθυνση e-mail.","Δεν έχετε εισάγει έγκυρο κωδικό πρόσβασης.","Έχετε ήδη εγγραφεί ως διαχειριστής οινοποιείου.",
+	"Έχετε ήδη εγγραφεί ως χρήστης. Μπορείτε να στείλετε μήνυμα στο winerouteseu@gmail.com για αναβάθμιση λογαριασμού σας.","Προβολές","Επισκέψεις ιστοτόπου",
+	"Πρόβλημα σύνδεσης με το server της εφαρμογής.","Η εγγραφή σας έγινε με επιτυχία. Σας εστάλη ένα μήνυμα για να ενεργοποιήσετε το λογαριασμό σας.",
+	"Έχετε πληκτρολογήσει διαφορετικό κωδικό.","Τα στοιχεία ενημερώθηκαν επιτυχώς.","Η ενημέρωση στοιχείων απέτυχε.","Δεν υπάρχουν στοιχεία.",
+	"Επιτυχής αποστολή μηνύματος.","Δε βρέθηκε η διεύθυνση e-mail.", "Επιτυχής μεταφόρτωση αρχείων","Πρέπει να συμπληρωθούν όλα τα πεδία με έγκυρο τρόπο."];
+var englishnames = ["You have not inserted a valid e-mail address.","You have not inserted a valid password.","You have already been registered as winery administrator.",
+	"You have already been registered as a user. Please send a message to winerouteseu@gmail.com for upgrading your account.","Views","Site clicks",
+	"There is a problem connecting to the application's server.","You have been successfully registered. A message has been sent to you to activate your account.",
+	"You have inserted a different password.","Details successfully updated.","Failure to update details.","There is no data.","Message successfully sent.","The e-mail address has not been found.",
+	"Files successfully uploaded.","All fields must be completed in a valid way."];
+var germannames = ["Sie haben keine gültige E-Mail-Adresse eingegeben.", "Sie haben kein gültiges Passwort eingegeben.", 
+	"Sie sind bereits als Kellereimanager registriert.", "Sie sind bereits als Benutzer registriert. Sie können eine E-Mail an winerouteseu@gmail.com senden, um Ihr Konto zu aktualisieren.", 
+	"Views", "Site Visits", "Verbindungsproblem mit dem Anwendungsserver.", 
+	"Ihre Registrierung war erfolgreich. Es wurde eine Nachricht an Sie gesendet, um Ihr Konto zu aktivieren.", "Sie haben ein anderes Passwort eingegeben.", 
+	"Elemente wurden erfolgreich aktualisiert.", "Daten konnten nicht aktualisiert werden.", "Keine Daten.", 
+	"Msgstr Nachricht erfolgreich senden.", "E-Mail-Adresse nicht gefunden.", "Dateien erfolgreich hochgeladen","Alle Felder müssen gültig ausgefüllt werden."]
+var spanishnames = [ "No ha ingresado una dirección de correo electrónico válida.", "No ha ingresado una contraseña válida.", 
+	"Ya estás registrado como gerente de bodega.", "Ya está registrado como usuario. Puede enviar un correo electrónico a winerouteseu@gmail.com para actualizar su cuenta", 
+	"Vistas", "Visitas al sitio", "Problema de conexión del servidor de aplicaciones.", 
+	"Su registro ha sido exitoso. Se le ha enviado un mensaje para activar su cuenta.", "Has ingresado una contraseña diferente.", 
+	"Elementos actualizados correctamente.", "Error al actualizar los datos.", "Sin datos.", "Enviando mensaje con éxito.", 
+	"Dirección de correo electrónico no encontrada.", "Carga exitosa de archivos","Todos los campos deben completarse de manera válida."];
+var frenchnames = ["Vous n&#39;avez pas entré d&#39;adresse e-mail valide.", "Vous n&#39;avez pas entré de mot de passe valide.", 
+	"Vous êtes déjà inscrit en tant que responsable de cave.", "Vous êtes déjà inscrit en tant qu&#39;utilisateur. Vous pouvez envoyer un e-mail à winerouteseu@gmail.com pour mettre à niveau votre compte.", 
+	"Views", "Visites sur le site", "Problème de connexion au serveur d&#39;applications.", 
+	"Votre inscription a réussi. Un message vous a été envoyé pour activer votre compte.", "Vous avez entré un mot de passe différent.", 
+	"Éléments mis à jour avec succès.", "Impossible de mettre à jour les données.", "Aucune donnée.", 
+	"Envoi du message avec succès.", "Adresse e-mail introuvable.", "Télécharger des fichiers avec succès","Tous les champs doivent être complétés de manière valide."]
+var italiannames = [ "Non hai inserito un indirizzo email valido.", "Non hai inserito una password valida.", 
+	"Sei già registrato come gestore della cantina.", "Sei già registrato come utente. Puoi inviare un&#39;e-mail a winerouteseu@gmail.com per aggiornare il tuo account.", 
+	"Visualizzazioni", "Visite al sito", "Problema di connessione al server delle applicazioni.", 
+	"La tua registrazione è andata a buon fine. Ti è stato inviato un messaggio per attivare il tuo account.", 
+	"Hai inserito una password diversa.", "Articoli aggiornati correttamente.", "Impossibile aggiornare i dati.", 
+	"Nessun dato.", "Invio corretto del messaggio.", "Indirizzo e-mail non trovato.", "Caricamento riuscito dei file","Tutti i campi devono essere compilati in modo valido."]
+var japanesenames = ["「有効なメールアドレスを入力していません。」","「有効なパスワードが入力されていません。」 ","「すでにワイナリーマネージャーとして登録されています。」", 
+	"「すでにユーザーとして登録されています。winerouteseu@ gmail.comにメールを送信して、アカウントをアップグレードできます。」", "Views", "Site Visits", 
+	"アプリケーションサーバー接続の問題。", "「登録は成功しました。アカ​​ウントを有効にするためのメッセージが送信されました。」", "「別のパスワードを入力しました。」 ", "アイテムは正常に更新されました。」", 
+	"「データの更新に失敗しました。」", "「データなし」。", "「メッセージを正常に送信しました。」", "「メールアドレスが見つかりません。」", 
+	"「ファイルのアップロードに成功しました」","すべてのフィールドは有効な方法で入力する必要があります。"];
+var portuguesenames = ["Você não digitou um endereço de email válido.", "Você não digitou uma senha válida.", 
+	"Você já está registrado como gerente de vinícola.", "Você já está registrado como usuário. Você pode enviar um e-mail para winerouteseu@gmail.com para atualizar sua conta.", 
+	"Visualizações", "Visitas ao Site", "Problema na conexão do servidor de aplicativos.", 
+	"Seu registro foi bem-sucedido. Uma mensagem foi enviada a você para ativar sua conta.", "Você digitou uma senha diferente.", 
+	"Itens atualizados com sucesso.", "Falha ao atualizar dados.", "Sem dados.", "Enviando mensagem com sucesso.", 
+	"Endereço de email não encontrado.", "Upload de arquivos com sucesso","Todos os campos devem ser preenchidos de forma válida."];
+var russiannames = ["Вы не ввели действительный адрес электронной почты.", "Вы не ввели действительный пароль.", 
+	"Вы уже зарегистрированы как менеджер винодельни.", "Вы уже зарегистрированы как пользователь. Вы можете написать winerouteseu@gmail.com, чтобы обновить свою учетную запись.", 
+	"Views", "Посещения сайта", "Проблема соединения с сервером приложений.", 
+	"Ваша регистрация прошла успешно. Вам отправлено сообщение для активации учетной записи.", "Вы ввели другой пароль.", "Элементы обновлены успешно.", 
+	"Не удалось обновить данные.", "Нет данных.", "Отправка сообщения успешно.", "Адрес электронной почты не найден.", 
+	"Загрузка файлов успешно","Все поля должны быть заполнены корректно."];
+var chinesenames = [ "您尚未输入有效的电子邮件地址", "您尚未输入有效的密码。", "您已经注册为酒庄经理。", "您已经注册为用户", "您可以发送电子邮件至winerouteseu@gmail.com升级您的帐户。", 
+	"视图", "站点访问", "应用程序服务器连接问题。", "您的注册已成功。已向您发送一条消息以激活您的帐户。", "您输入了另一个密码。", "项目已成功更新。", "无法更新数据。", "没有数据。", 
+	"成功发送消息。", "找不到电子邮件地址。", "成功上传文件","所有字段必须以有效方式填写。"];
